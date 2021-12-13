9b6396f74683e94f098bfc063d0eee2ab300245f
krpraveen0 <pkec151015@gmail.com>
2021-12-13 19:28:30 +0530

    api-v1

--- shopping_cart/api_app/urls.py
+++ shopping_cart/api_app/urls.py
@@ -1,7 +1,9 @@
 #application level url
 from django.urls import path
-from .views import ShoppingCart
+from .views import ShoppingCart, ShoppingCartUpdate
 
 urlpatterns = [
-    path('cart-items/',ShoppingCart.as_view())
+    path('cart-items/',ShoppingCart.as_view()),
+    #adding URL-variable <int:item_id> - dynamic component of the url
+    path('update-item/<int:item_id>',ShoppingCartUpdate.as_view()),
 ]
--- shopping_cart/api_app/views.py
+++ shopping_cart/api_app/views.py
@@ -15,16 +15,66 @@ class ShoppingCart(View):
         p_name = data.get('product_name')
         p_price = data.get('product_price')
         p_quantity = data.get('product_quantity')
-        print(p_name)
+
         product_data = {
             'product_name': p_name,
             'product_price': p_price,
             'product_quantity':p_quantity,
         }
-        print(product_data)
 
         cart_item = CartItem.objects.create(**product_data)
         data = {
-        "message": f"New item added to the cart"
+        "message": "New item added to the cart"
         }
         return JsonResponse(data,status=201)
+
+    #retrieving the data from the database using the GET request handler
+    def get(self,request):
+        #count() method counts the number of occurences in the databases
+        item_counts = CartItem.objects.count()
+        #all() method retrieves all the objects into a list of entities.
+        items = CartItem.objects.all()
+
+        items_data = []
+        for item in items:
+            items_data.append({
+                'product_name':item.product_name,
+                'product_price': item.product_price,
+                'product_quantity':item.product_quantity
+            })
+        #creating the data in the form of python dictionary
+        data = {
+            'items': items_data,
+            'count': item_counts,
+        }
+        #returning the data as a JSON response.
+        return JsonResponse(data)
+
+
+#updating a cart item by using the patch method
+@method_decorator(csrf_exempt,name='dispatch')
+class ShoppingCartUpdate(View):
+    def patch(self,request, item_id):
+        data = json.loads(request.body.decode('utf-8'))
+        item = CartItem.objects.get(id=item_id)
+        #changing the quantity of a product
+        item.product_quantity = data['product_quantity']
+        #using save() method to save the change in the database
+        item.save()
+
+        data = {
+            'message': f'Item {item_id} has been updated'
+        }
+
+        return JsonResponse(data)
+
+    #deleting the items from the cart
+    def delete(self,request,item_id):
+        item = CartItem.objects.get(id=item_id)
+        #delete() method is used to delete the item on which it is called
+        item.delete()
+
+        data = {
+            'message': f'Item {item_id} has been deleted'
+        }
+        return JsonResponse(data)
